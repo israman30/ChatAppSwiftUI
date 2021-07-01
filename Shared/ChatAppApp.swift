340b90f2440825d92700cd23461e17bf150b271f
israman <israelmanzo814@gmail.com>
2021-06-30 22:18:27 -0400

    Initial Commit

---
+++ Shared/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  Shared
+//
+//  Created by Israel Manzo on 6/30/21.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
